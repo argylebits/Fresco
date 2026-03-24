0c939b1ab39a281a5d22b06768a925c7023f85ee
Jim Schultz <jim@argylebits.com>
2026-03-24 16:30:09 -0500

    Add swift-configuration dependency to FrescoCore
    Required for reading environment variables via EnvironmentVariablesProvider
    in GenerateService.

--- FrescoCore/Package.swift
+++ FrescoCore/Package.swift
@@ -11,12 +11,14 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-crypto.git", from: "3.0.0"),
+        .package(url: "https://github.com/apple/swift-configuration.git", from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "FrescoCore",
             dependencies: [
                 .product(name: "Crypto", package: "swift-crypto"),
+                .product(name: "Configuration", package: "swift-configuration"),
             ]
         ),
         .testTarget(
