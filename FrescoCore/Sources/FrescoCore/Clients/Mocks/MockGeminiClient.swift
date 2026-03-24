2fd551fcefba7ac2bcd10b93312d988c4635e0a5
Jim Schultz <jim@argylebits.com>
2026-03-24 05:36:13 -0500

    implement MockGeminiClient conforming to GeminiClientProtocol
    Configurable mock with success/failure behaviours for use in tests.

---
+++ FrescoCore/Sources/FrescoCore/Clients/Mocks/MockGeminiClient.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+final class MockGeminiClient: GeminiClientProtocol, Sendable {
+    enum Behaviour: Sendable {
+        case success(Data)
+        case failure(FrescoError)
+    }
+
+    let behaviour: Behaviour
+
+    init(behaviour: Behaviour) {
+        self.behaviour = behaviour
+    }
+
+    func generateImage(prompt: String) async throws(FrescoError) -> Data {
+        switch behaviour {
+        case .success(let data):
+            return data
+        case .failure(let error):
+            throw error
+        }
+    }
+}
